91cb1aac8824c5981fd9551d14bec9dcd8be27a8
Marvin Menchú <mmenchu@outlook.com>
2019-06-07 11:50:18 -0600

    agregando funcion para calcular y dibujar,area y perimetro de un triangulo

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@
 /* prototipo de funciones */
 void crearCuadrado();
 void crearCirculo();
-
+void crearTriangulo();
+void crearRectangulo();
+void crearRombo();
+void crearPentagono();
+void crearHexagono();
+void crearTrapecio();
+void CrearParalelogramo();
 void despliegaMenu();
 
 
@@ -47,7 +53,35 @@ class Cuadrado : public figuraGeometrica {
         double alto,ancho;
 };
 
-
+class Triangulo : public figuraGeometrica {
+    public: 
+        Triangulo(double p_ladoA, double p_ladoB, double  p_ladoC, double p_base, double p_altura){
+            ladoA = p_ladoA;
+            ladoB = p_ladoB;
+            ladoC = p_ladoC;
+            base = p_base;
+            altura = p_altura;
+        };
+        double calculaArea(){
+             area = ((base * altura )/2);
+            return area;
+        };
+        double calculaPerimetro(){
+            perimetro = ladoA + ladoB + ladoC;
+            return perimetro;
+        };
+        void dibujar(){
+            
+             int gd = DETECT, gm; 
+            int arr[] = {320, 150, 400, 250,270, 250, 320, 150}; 
+            initgraph(&gd, &gm, ""); 
+            drawpoly(4, arr);    
+            getch();  
+            closegraph(); 
+        };
+    private:
+    double ladoA,ladoB,ladoC,base,altura;
+};
 
 
 int main(int argc, char** argv) {
@@ -66,12 +100,12 @@ void despliegaMenu(){
     do
     {   
         printf("\n Proyecto Calculadora de Areas para Figuras Geometricas");
-        printf( "\n   1. Calcular el Area y el perimetro de un cuadrado.", 163 );
-        
-        printf( "\n   4. Salir." );
-        printf( "\n\n   Introduzca opci%cn (1-4): ", 162 );
+        printf("\n   1. Calcular el Area y el perimetro de un cuadrado.", 163 );
+        printf("\n   2. Calcular el area y el perimetro de un triangulo");
+        printf("\n   4. Salir." );
+        printf("\n\n   Introduzca opci%cn (1-4): ", 162 );
 
-        scanf( "%d", &opcion );
+        scanf("%d", &opcion );
 
         /* Inicio del anidamiento */
 
@@ -82,7 +116,7 @@ void despliegaMenu(){
                     break;
 
             case 2: 
-                    crearCirculo();
+                    crearTriangulo();
                     break;
          }
 
@@ -97,12 +131,31 @@ void crearCuadrado(){
      scanf( "%lf", &ladoCuadrado );
 
      Cuadrado nuevoCuadrado(ladoCuadrado);
-     printf("\n   El Area del cuadrado es: %f", nuevoCuadrado.calculaArea());
-     printf("\n   El perimetro del cuadrado es: %f", nuevoCuadrado.calculaPerimetro());
+     printf("\n   El Area del cuadrado es: %.2f", nuevoCuadrado.calculaArea());
+     printf("\n   El perimetro del cuadrado es: %.2f", nuevoCuadrado.calculaPerimetro());
       nuevoCuadrado.dibujar();
      printf("\n");
 }
 
+void crearTriangulo(){
+    double ladoA,ladoB,ladoC,base,altura;
+    printf("\n   Ingresa el lado A ");
+    scanf("%lf",&ladoA);
+    printf("\n   Ingresa el lado B ");
+    scanf("%lf",&ladoB);
+    printf("\n   Ingresa el lado C ");
+    scanf("%lf",&ladoC);
+    printf("\n   Ingresa el base ");
+    scanf("%lf",&base);
+    printf("\n   Ingresa el altura ");
+    scanf("%lf",&altura);
+    Triangulo nuevoTriangulo(ladoA,ladoB,ladoC,base,altura);
+    printf("\n\n\n   El area del triangulo es: %.2f",nuevoTriangulo.calculaArea());
+    printf("\n   El perimetro del triangulo es:  %.2f",nuevoTriangulo.calculaPerimetro());
+    nuevoTriangulo.dibujar();
+
+
+}
 
 void crearCirculo(){
 
